0a7e7976f58f492e66d6d84ea96a6e5d94529067
ma-sss <120617383+ma-sss@users.noreply.github.com>
2024-04-15 17:33:22 +0900

    Update 20240105081830_devise_token_auth_create_admins.rb

--- db/migrate/20240105081830_devise_token_auth_create_admins.rb
+++ db/migrate/20240105081830_devise_token_auth_create_admins.rb
@@ -40,7 +40,7 @@ class DeviseTokenAuthCreateAdmins < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    add_index :admins, :email,                unique: true
+    add_index :admins, :email, unique: true, length: 191
     add_index :admins, [:uid, :provider],     unique: true
     # add_index :admins, :reset_password_token, unique: true
     # add_index :admins, :confirmation_token,   unique: true
