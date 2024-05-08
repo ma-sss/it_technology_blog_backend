f60259a79c378208f94c80e5074393a546560dbc
ma-sss <mskk0522@icloud.com>
2024-05-08 20:46:13 +0900

    seeds.rb 修正（本番用）

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,38 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-count = 0
+# count = 0
 
-1.times do |i|
-  admin = Admin.create!(email: "admin#{i}@example.com", password: "Password123", password_confirmation: "Password123")
+#1.times do |i|
+  #admin = Admin.create!(email: "admin#{i}@example.com", password: "<%= ENV['ADMIN_PASS'] %>", password_confirmation: "<%= ENV['ADMIN_PASS'] %>")
+  Admin.create!(email: "admin123@example.com", password: "<%= ENV['ADMIN_PASS'] %>", password_confirmation: "<%= ENV['ADMIN_PASS'] %>")
 
-    3.times do |j|
-        post = Post.create!(admin_id: admin.id, category: "tentative", title: "仮の投稿タイトル#{j}", content: "仮の投稿内容#{j}")
+    #3.times do |j|
+    #    post = Post.create!(admin_id: admin.id, category: "tentative", title: "仮の投稿タイトル#{j}", content: "仮の投稿内容#{j}")
 
-        2.times do |k|
-            Comment.create!(post_id: post.id, admin_id: admin.id, user_id: nil, text: "投稿内容いいね！")
+    #    2.times do |k|
+    #        Comment.create!(post_id: post.id, admin_id: admin.id, user_id: nil, text: "投稿内容いいね！")
 
-            2.times do |l|
-                comment = Comment.last
-                Reply.create!(comment_id: comment.id, admin_id: admin.id, user_id: nil, text: "コメント内容いいね！")
+    #        2.times do |l|
+    #            comment = Comment.last
+    #            Reply.create!(comment_id: comment.id, admin_id: admin.id, user_id: nil, text: "コメント内容いいね！")
 
-                count += 1
-                user = User.create!(name: "一般ユーザー#{count}")
-                Reply.create!(comment_id: comment.id, admin_id: nil, user_id: user.id, text: "コメント内容いいね！")
-            end
+    #            count += 1
+    #            user = User.create!(name: "一般ユーザー#{count}")
+    #            Reply.create!(comment_id: comment.id, admin_id: nil, user_id: user.id, text: "コメント内容いいね！")
+    #        end
 
-            count += 1
-            user = User.create!(name: "一般ユーザー#{count}")
-            Comment.create!(post_id: post.id, admin_id: nil, user_id: user.id, text: "投稿内容いいね！")
+    #        count += 1
+    #        user = User.create!(name: "一般ユーザー#{count}")
+    #        Comment.create!(post_id: post.id, admin_id: nil, user_id: user.id, text: "投稿内容いいね！")
 
-            2.times do |m|
-                comment = Comment.last
-                Reply.create!(comment_id: comment.id, admin_id: admin.id, user_id: nil, text: "コメント内容いいね！")
+    #        2.times do |m|
+    #            comment = Comment.last
+    #            Reply.create!(comment_id: comment.id, admin_id: admin.id, user_id: nil, text: "コメント内容いいね！")
 
-                count += 1
-                user = User.create!(name: "一般ユーザー#{count}")
-                Reply.create!(comment_id: comment.id, admin_id: nil, user_id: user.id, text: "コメント内容いいね！")
-            end
-        end
-    end
-end
+    #            count += 1
+    #            user = User.create!(name: "一般ユーザー#{count}")
+    #            Reply.create!(comment_id: comment.id, admin_id: nil, user_id: user.id, text: "コメント内容いいね！")
+    #        end
+    #    end
+    #end
+#end
