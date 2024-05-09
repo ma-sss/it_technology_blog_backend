831f0290e8703b6db372f1cbf2ced57a501a1efc
ma-sss <mskk0522@icloud.com>
2024-05-09 21:15:56 +0900

    postモデルのカテゴリーに説明（explanation）を追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :admin
     has_many :comment, dependent: :destroy
-    enum category: { frontend: 'frontend', backend: 'backend', other: 'other', tentative: 'tentative' }
+    enum category: { explanation: 'explanation', frontend: 'frontend', backend: 'backend', other: 'other', tentative: 'tentative' }
     validates :category, presence: true
     validates :admin_id, presence: true
     validates :title, presence: true,
